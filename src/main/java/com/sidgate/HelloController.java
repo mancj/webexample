8d449c7a2cbaad37e3ebe1d4b04d32657f7ac402
Siddharth Gargate <siddharth@careskore.com>
2016-09-20 11:32:41 -0700

    working project

---
+++ src/main/java/com/sidgate/HelloController.java
@@ -0,0 +1,14 @@
+package com.sidgate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    
+}
---
+++ src/main/java/com/sidgate/WebexampleApplication.java
@@ -0,0 +1,12 @@
+package com.sidgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebexampleApplication.class, args);
+	}
+}
